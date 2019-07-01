dda87a3a3ee82e73bfacbd2e02680818528ed057
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-07-01 17:09:03 +0100

    correct tests

--- test/index.test.js
+++ test/index.test.js
@@ -6,11 +6,11 @@ const kappa = require('kappa-core')
 const View = require('../')
 
 const db = memdb()
-
 const core = kappa(ram, { valueEncoding: 'json' })
 
-var indexes = [{ key: 'typ', value: [['value', 'type'], ['value', 'timestamp']]  }]
-const view = View(db, core, indexes)
+var indexes = [{ key: 'typ', value: [['value', 'type'], ['value', 'timestamp']] }]
+
+const view = View(db, core, { indexes })
 core.use('query', 1, view)
 
 var seeds = [0, 1, 3, 5, 2, 4].map((i) => ({
@@ -19,6 +19,7 @@ var seeds = [0, 1, 3, 5, 2, 4].map((i) => ({
   content: { body: `Hello World ${i}` }
 }))
 
+
 seedDB(() => {
   test('orders messages by type then timestamp', function (assert) {
     const opts = { query: [{ $filter: { value: { type: 'chat/message' }  } }] }
@@ -26,7 +27,6 @@ seedDB(() => {
     pull(
       core.api.query.read(opts),
       pull.collect((err, msgs) => {
-        if (err) return console.error(err)
         assert.deepEqual(
           msgs.map((msg) => msg.value),
           seeds.sort((a, b) => a.timestamp > b.timestamp ? +1 : -1),
