5b54f230a26f37b3c20f38dece053d6e5e507972
Kieran Gibb <kg@blockades.org>
2020-01-14 12:46:35 +0000

    first test passes

--- index.js
+++ index.js
@@ -16,134 +16,127 @@ module.exports = function KappaViewQuery (db = memdb(), opts = {}) {
   const {
     indexes = [],
     validator = (msg) => msg,
-    keyEncoding = charwise
+    keyEncoding = charwise,
+    getFeed,
   } = opts
 
-  const view = {
+  return {
     maxBatch: opts.maxBatch || 100,
-
-    map: (msgs, next) => {
-      var ops = []
-
-      msgs.forEach((msg) => {
-        msg = validator(msg)
-        if (!msg) return
-
-        indexes.forEach((idx) => {
-          var indexKeys = getIndexValues(msg, idx.value)
-
-          if (indexKeys.length) {
-            ops.push({
-              type: 'put',
-              key: [idx.key, ...indexKeys, msg.seq, msg.key],
-              value: [msg.key, msg.seq].join('@'),
-              keyEncoding,
-            })
-          }
-
-          function getIndexValues (msg, value) {
-            var child = value[0]
-            if (Array.isArray(child)) {
-              return value
-                .map((val) => getIndexValues(msg, val))
-                .reduce((acc, arr) => [...acc, ...arr], [])
-                .filter(Boolean)
-            } else if (typeof child === 'string') {
-              return [value.reduce((obj, val) => obj[val], msg)]
-                .filter(Boolean)
-            } else return []
-          }
-        })
-      })
-
-      db.batch(ops, next)
-    },
-    indexed: (msgs) => {
-      debug(`indexing ${JSON.stringify(msgs, null, 2)}`)
-
-      msgs.forEach((msg) => {
-        events.emit('update', msg)
-      })
-    },
+    map,
+    indexed,
     api: {
-      read: (core, _opts) => {
-        var __opts = view.api.explain(core, _opts)
-        var source = __opts.createStream(__opts)
-        return Filter(source, _opts)
-      },
-      explain: (core, _opts) => {
-        var explain = Explain(indexes.map((idx) => Object.assign(idx, {
-          exact: typeof idx.exact === 'boolean' ? idx.exact : false,
-          createStream: (__opts) => {
-            var thru = through.obj(function (msg, enc, next) {
-              if (msg.sync) {
-                this.push(msg)
-                return next()
-              }
-
-              var msgId = msg.value
-              var [ feedId, sequence ] = msgId.split('@')
-              var feed = core._logs.feed(feedId)
-              var seq = Number(sequence)
-
-              feed.get(seq, (err, value) => {
-                if (err) return next()
-
-                var msg = validator({
-                  key: feed.key.toString('hex'),
-                  seq,
-                  value
-                })
-
-                if (!msg) return next()
-                this.push(msg)
-                next()
-              })
-            })
-
-            var streamOpts = Object.assign(__opts, {
-              lte: [idx.key, ...__opts.lte],
-              gte: [idx.key, ...__opts.gte],
-              keyEncoding,
-              keys: true,
-              values: true
-            })
-
-            var stream = __opts.live
-              ? liveStream(db, streamOpts)
-              : db.createReadStream(streamOpts)
-
-            stream.pipe(thru)
-
-            return thru
-          }
-        })))
-
-        return explain(_opts)
-      },
-      add: (core, _opts) => {
-        var isValid = _opts && isFunction(_opts.createStream) && Array.isArray(_opts.index || _opts.value)
-        if(!isValid) throw new Error('kappa-view-query.add: expected opts { index, createStream }')
-        _opts.value = _opts.index || _opts.value
-        indexes.push(_opts)
-      },
-      onUpdate: (core, cb) => {
-        events.on('update', cb)
-      },
-      storeState: (state, cb) => {
-        state = state.toString('base64')
-        db.put('state', state, cb)
-      },
-      fetchState: (cb) => {
-        db.get('state', function (err, state) {
-          if (err && err.notFound) cb()
-          else if (err) cb(err)
-          else cb(null, Buffer.from(state, 'base64'))
-        })
-      },
+      read,
+      explain,
+      add,
+      onUpdate: (core, cb) => events.on('update', cb),
       events
     }
   }
 
-  return view
+  function explain (core, _opts) {
+    var expl = Explain(indexes.map((idx) => Object.assign(idx, {
+      exact: typeof idx.exact === 'boolean' ? idx.exact : false,
+      createStream: (__opts) => {
+        var thru = through.obj(function (msg, enc, next) {
+          if (msg.sync) {
+            this.push(msg)
+            return next()
+          }
+
+          getFeed(msg, (err, { feed, seq }) => {
+            if (err) return next()
+
+            feed.get(seq, (err, value) => {
+              if (err) return next()
+
+              var msg = validator({
+                key: feed.key.toString('hex'),
+                seq,
+                value
+              })
+
+              if (!msg) return next()
+              this.push(msg)
+              next()
+            })
+          })
+        })
+
+        var streamOpts = Object.assign(__opts, {
+          lte: [idx.key, ...__opts.lte],
+          gte: [idx.key, ...__opts.gte],
+          keyEncoding,
+          keys: true,
+          values: true
+        })
+
+        var stream = __opts.live
+          ? liveStream(db, streamOpts)
+          : db.createReadStream(streamOpts)
+
+        stream.pipe(thru)
+
+        return thru
+      }
+    })))
+
+    return expl(_opts)
+  }
+
+  function read (core, _opts) {
+    var __opts = explain(core, _opts)
+    var source = __opts.createStream(__opts)
+    return Filter(source, _opts)
+  }
+
+  function add (core, _opts) {
+    var isValid = _opts && isFunction(_opts.createStream) && Array.isArray(_opts.index || _opts.value)
+    if(!isValid) throw new Error('kappa-view-query.add: expected opts { index, createStream }')
+    _opts.value = _opts.index || _opts.value
+    indexes.push(_opts)
+  }
+
+  function map (msgs, next) {
+    var ops = []
+
+    msgs.forEach((msg) => {
+      msg = validator(msg)
+      if (!msg) return
+
+      indexes.forEach((idx) => {
+        var indexKeys = getIndexValues(msg, idx.value)
+
+        if (indexKeys.length) {
+          ops.push({
+            type: 'put',
+            key: [idx.key, ...indexKeys, msg.seq, msg.key],
+            value: [msg.key, msg.seq].join('@'),
+            keyEncoding,
+          })
+        }
+
+        function getIndexValues (msg, value) {
+          var child = value[0]
+          if (Array.isArray(child)) {
+            return value
+              .map((val) => getIndexValues(msg, val))
+              .reduce((acc, arr) => [...acc, ...arr], [])
+              .filter(Boolean)
+          } else if (typeof child === 'string') {
+            return [value.reduce((obj, val) => obj[val], msg)]
+              .filter(Boolean)
+          } else return []
+        }
+      })
+    })
+
+    db.batch(ops, next)
+  }
+
+  function indexed (msgs) {
+    msgs.forEach((msg) => {
+      events.emit('update', msg)
+    })
+  }
+
 }
